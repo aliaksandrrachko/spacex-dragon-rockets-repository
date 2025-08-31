134dd3d3ced16de5eeade10c692090a15cc89f59
aliaksandr.rachko <rachko.a.ch@gmail.com>
2025-08-31 20:01:51 +0200

    Add @UtilityClass annotation to factory classes

--- core/src/main/java/io/github/aliaksandrrachko/spacex/dragon/rockets/repository/core/api/MissionRepositoryFactory.java
+++ core/src/main/java/io/github/aliaksandrrachko/spacex/dragon/rockets/repository/core/api/MissionRepositoryFactory.java
@@ -1,7 +1,9 @@
 package io.github.aliaksandrrachko.spacex.dragon.rockets.repository.core.api;
 
 import io.github.aliaksandrrachko.spacex.dragon.rockets.repository.core.impl.MissionRepositoryInMemoryImpl;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 public class MissionRepositoryFactory {
   public static MissionRepository createInMemory() {
     return new MissionRepositoryInMemoryImpl();
--- core/src/main/java/io/github/aliaksandrrachko/spacex/dragon/rockets/repository/core/api/RocketRepositoryFactory.java
+++ core/src/main/java/io/github/aliaksandrrachko/spacex/dragon/rockets/repository/core/api/RocketRepositoryFactory.java
@@ -1,7 +1,9 @@
 package io.github.aliaksandrrachko.spacex.dragon.rockets.repository.core.api;
 
 import io.github.aliaksandrrachko.spacex.dragon.rockets.repository.core.impl.RocketRepositoryInMemoryImpl;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 public class RocketRepositoryFactory {
   public static RocketRepository createInMemory() {
     return new RocketRepositoryInMemoryImpl();
